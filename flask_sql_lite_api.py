6257a817fb398c905d7b622f13aea1b6318f78b8
SamSk <sskurikhin3@gmail.com>
2025-01-18 16:08:31 +0100

    initial commit with home endpoint and DB setup

---
+++ flask_sql_lite_api.py
@@ -0,0 +1,36 @@
+import sqlite3
+from flask import Flask,jsonify,request,Response as re
+
+
+
+app=Flask(__name__)
+
+def connect_db():
+    db=sqlite3.connect('groceries.db')
+    db.execute('CREATE TABLE IF NOT EXISTS Items (item_id INTEGER PRIMARY KEY, item_name TEXT, item_price REAL, item_quant)')
+    db.commit()
+
+connect_db()
+
+
+@app.route('/')
+@app.route('/home')
+def home_page():
+    endpoint={
+        '/available_items(GET)':" lists all available items in the Items DB with the quantity <0",
+        '/all_items(GET) ':" lists all items in the DB regardless quantity",
+        '/add_item(POST) ':" adds an item to the db",
+        '/get_item_by_id/<id>(GET) ':" displays an item of the id selected",
+        '/available_items_total_cost ':" provides a sum of all products in the 'store' ",
+        '/update_item/<id>(PUT) ':" updates item based on ID",
+        '/delete_item/<id>(DELETE) ':" deletes item based on ID"
+    }
+    msg=f'Wellcome to the sotore!\n\nOur enpoints:\n'+'\n'+"\n".join([f"{x}:{y}" for x,y in endpoint.items()])
+    return re(msg,mimetype='text/plain')
+
+
+
+if __name__=='__main__':
+    app.run(debug=True)
+
+ 
